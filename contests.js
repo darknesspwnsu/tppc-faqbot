b0607a33c1b9e87ce03baed732a9ba1b1e2f751e
darknesspwnsu <dev0001337@gmail.com>
2025-12-28 18:57:06 -0800

    remove old unused contests file

--- contests.js
+++
@@ -1,7 +0,0 @@
-// contests.js (shim)
-// NOTE: Temporary compatibility layer. Will be removed later.
-
-// export { registerContests } from "./contests/contests.js";
-
-// Back-compat exports (these existed previously)
-// export { collectEntrantsByReactions, installReactionHooks } from "./contests/reaction_contests.js";
